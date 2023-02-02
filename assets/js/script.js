1816bf17e234f5c88b3aabf48eea43bcd923e5a1
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-02-02 00:20:16 +0000

    using a for loop to add timeblock row

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,29 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 //   var toDay = moment().format('dddd, MMMM Do');
 //   currentDisplayEl.text(toDay);
 // }
+
+let hours = [
+    ["9AM", moment()],
+    ["10AM", moment()],
+    ["11AM", moment()],
+    ["12PM", moment()],
+    ["1PM", moment()],
+    ["2PM", moment()],
+    ["3PM", moment()],
+    ["4PM", moment()],
+    ["5PM", moment()],
+];
+
+var currentTime;
+for (let i = 0; i < hours.length; i++) {
+    const [time, hour] = hours[i];
+    var schedulTime = hours[i][0];
+    $(".time-block") .append(
+            `<div class="row my-row">
+            <div class="col col-1 hour">${time}</div>
+                <input class="col-10 description textarea past type="text" id="content ">
+                <button class = "saveBtn"> save </button>
+            </div>`
+        );
+
+};
