7e3535afd88014b68233fe80e74d337a40a27aa7
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-02-02 00:12:50 +0000

    ref dom elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// save reference to important DOM elements
+var currentDisplayEl = $('#currentDay');
+var saveButtonEl = $('.saveBtn');
+let currentHour = moment().format('hA');
+var savedTextDisplayEl = document.getElementById('.saved');
