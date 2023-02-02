2981cbb7ac3d05a2f839b719e8d1cb7a2ec0120a
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-02-02 00:14:38 +0000

    js for displaing the current day on the html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ var currentDisplayEl = $('#currentDay');
 var saveButtonEl = $('.saveBtn');
 let currentHour = moment().format('hA');
 var savedTextDisplayEl = document.getElementById('.saved');
+
+//var currentHour = Date.now().getHours(); //get current time hour
+
+// handle displaying the the date
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+// function displayTime() {
+//   var toDay = moment().format('dddd, MMMM Do');
+//   currentDisplayEl.text(toDay);
+// }
